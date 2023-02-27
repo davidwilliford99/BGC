f5b0ae690c73d2360a6a431e98caa44e39206e2d
Jwelb <jordanwelborn2002@gmail.com>
2023-02-27 14:37:49 -0500

    Installed npm app
    I did the simple npm install command line. I provided comments with what files in the project does what.

---
+++ app/src/index.js
@@ -0,0 +1,20 @@
+/*
+This is where your components or javascript gets rendered to the webpage
+*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
