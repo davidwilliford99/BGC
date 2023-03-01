aebd31c50220d08ad59a951299ef7c840dc05365
Jwelb <jordanwelborn2002@gmail.com>
2023-03-01 14:38:22 -0500

    Commit
    Commit

--- app/src/App.js
+++ app/src/App.js
@@ -7,22 +7,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className='Nav' >
+      <a className='nav-logo' href='#'>
+      </a>
+      <button 
+      className='nav-toggler' 
+      type='button' 
+      data-toggle='collapse'
+      data-target='#navNavDropdown'
+      aria-controls='navNavDropdown'
+      aria-expanded='false'
+      aria-label='Toggle navigation' 
+      >
+      <span className='nav-toggler-icon'></span>
+      </button>
+
+    </nav>
   );
 }
 
