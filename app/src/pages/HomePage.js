a1794c322d7cfabeb1542b975a7f6e51c381da35
Jwelb <jordanwelborn2002@gmail.com>
2023-03-17 10:59:09 -0400

    Update HomePage.js

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,51 +1,49 @@
-import React from 'react'
-function HomePage(){
-    return(
-    <div class='card-wrapper'>
-    <div class="card">
-        <img class="card-img-top img-thumbnail" src="images/tim-kilby-x2qzLL3vdBs-unsplash.jpg" alt="alt text"/>
-        <div class="card-img-overlay">
-        <p class="text-light">
-            <h1>
-                "Lifting the veil of mismanaged information of
-                <small class='text-muted'> Bone Grafting"</small>
-            </h1>
-        </p>
-        <p class='text-light'>
-        <h1>- Mike Weddington</h1>
-        </p>
-    </div>
-        <div class="card-img-overlay position-absolute top-50">
-        <a href="#" class="btn btn-outline-primary btn-lg btn-block w-50"
-           data-bs-toggle="popover" data-bs-content="Edit image" data-bs-trigger="hover focus">
-            <h2 class="far fa-edit">Get Started</h2>
-        </a>
-    </div>
-    </div>
-    
-    <div class="pt-md">
-        <h2 class="card-title">Who we are</h2>
-        <h5 class="card-text">At Bone Graft Consortium, we help to take the guess work out of the crowded bone grafting space.</h5>      
-        <a href="/about" class="btn btn-outline-primary btn-sm"
-           data-bs-toggle="popover" data-bs-content="Edit image" data-bs-trigger="hover focus">
-            <h5 class="far fa-edit">Read More</h5>
-        </a>
-    </div>
+import React from 'react';
 
-    <div class="pt-md">
-    <h2>
-    What we provide
-    </h2>
-    <p>Place holder text</p>
-    </div>
-    <div>
-        <h1>
-           F.A.Q 
-        </h1>
-    </div>
+function HomePage() {
     
-</div>
-    )
+  return (
+    <div className='card-wrapper'>
+      <div className='card'>
+        <img
+          className='card-img-top img-thumbnail'
+          src='images/tim-kilby-x2qzLL3vdBs-unsplash.jpg'
+          alt='alt text'
+        />
+        <div className='card-img-overlay d-flex flex-column justify-content-center align-items-center'>
+          <h1 className='text-light text-center'>
+            "Lifting the veil of mismanaged information of{' '}
+            <small className='text-muted'>Bone Grafting"</small>
+          </h1>
+          <h1 className='text-light text-center'>- Mike Weddington</h1>
+          <a href='#' className='btn btn-primary btn-lg btn-get-started'>
+            Get Started
+          </a>
+        </div>
+      </div>
+
+      <div className='py-5'>
+        <h2 className='card-title'>Who we are</h2>
+        <p className='card-text'>
+          At Bone Graft Consortium, we help to take the guess work out of the
+          crowded bone grafting space.
+        </p>
+        <a
+          href='/about'
+          className='btn btn-outline-primary btn-sm'
+          data-bs-toggle='popover'
+          data-bs-content='Edit image'
+          data-bs-trigger='hover focus'>
+          <h5 className='far fa-edit'>Read More</h5>
+        </a>
+      </div>
+
+      <div className='py-5'>
+        <h2>F.A.Q</h2>
+        <p>Place holder text</p>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
