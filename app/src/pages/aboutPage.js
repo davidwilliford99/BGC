76f2b7a5c885fb32dd63938c6e4294f72b8c15c5
srazam <srazam5@gmail.com>
2023-03-20 09:32:00 -0400

    Update aboutPage.js

--- app/src/pages/aboutPage.js
+++ app/src/pages/aboutPage.js
@@ -3,26 +3,33 @@ import React from 'react'
 function AboutPage(){
     return(
         <div className='about-wrapper'>
-        <h1>Our Vision</h1>
-        <div className='about-text'>
-        <p>At The Bone Graft Consortium , we 
-            lift the veil of mismanaged, 
-            misrepresented, and misunderstood 
-            grafting categories so HCPs can easily 
-            and quickly make an informed decision. 
-            With seats on our board filled by 
-            executives from major grafting 
-            manufacturers, KOLs, insurance providers,
-             as well as small, biologic only companies, the BGC is the only 
-             completely comprehensive resource demanded by hospital systems and 
-             providers alike, in order to make an informed decision based on facts, 
-             not marketing, for best practices and patient specific needs.</p>
+            <h1>Our Vision</h1>
+                <p> At The Bone Graft Consortium , we 
+                    lift the veil of mismanaged, 
+                    misrepresented, and misunderstood 
+                    grafting categories so HCPs can easily 
+                    and quickly make an informed decision. 
+                    With seats on our board filled by 
+                    executives from major grafting 
+                    manufacturers, KOLs, insurance providers,
+                    as well as small, biologic only companies, the BGC is the only 
+                    completely comprehensive resource demanded by hospital systems and 
+                    providers alike, in order to make an informed decision based on facts, 
+                    not marketing, for best practices and patient specific needs.</p>
 
-             <h1>Statements</h1>
-             <p>Not all bone graft is created equally, nor should it have to be. </p>
+            <br></br>        
+
+        <div className = "card">
+            <div className = "card-body">  
+             <h1 className = "card-title">Statements</h1>
+             <p className = "card-text">Not all bone graft is created equally, nor should it have to be. </p>
              <p>At the Bone Graft Consortium(BGC) we help to take the guess work(and marketing 
                 confusion) out of the crowded bone grafting space</p>
-             
+            </div> 
+        </div> 
+
+            <br></br>
+
              <h1>Hospital Facing Statement</h1>
              <p>The fact based information provided by the BGC allows your hospital or ASC to make 
                 an informed decision, in a reasonable amount of time, as to the most relevant bone 
@@ -30,8 +37,6 @@ function AboutPage(){
                 and patient workflow but it also allows companies to know their place in the queue, without 
                 waiting an undetermined amount of time for a possible approval or denial based on non factual 
                 vetting processes(current value analysis systems across our country)</p>
-        </div>
-        
         </div>
     )
 }
