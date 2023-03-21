483e4bcb9ebafc62867463edd3d851e46a1943c8
Jwelb <jordanwelborn2002@gmail.com>
2023-03-21 13:53:32 -0400

    Updated Home

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function HomePage() {
-    
   return (
     <div className='card-wrapper'>
       <div className='card'>
@@ -38,10 +37,65 @@ function HomePage() {
         </a>
       </div>
 
-      <div className='py-5'>
-        <h2>F.A.Q</h2>
-        <p>Place holder text</p>
-      </div>
+      <section>
+  <h3 class="text-center mb-4 pb-2 text-primary fw-bold">FAQ</h3>
+  <p class="text-center mb-5">
+    Find the answers for the most frequently asked questions below
+  </p>
+
+  <div class="row">
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i>What does the BGC do that’s not currently being done in the industry</h6>
+      <p>
+        <strong><u>Absolutely!</u></strong> The Bone Graft Consortium is a platform that provides easily accessible, unbiased information about bone grafts for healthcare providers. It consolidates all bone grafts in a single website and app, allowing healthcare providers to make informed decisions based on facts. Manufacturers can register with the BGC and provide a comprehensive overview of their products, eliminating misinformation and misleading marketing claims. Hospitals and surgery centers can partner with the BGC to require manufacturers to join and gain entry into their facilities. The BGC provides a clear and concise pathway for manufacturers to gain visibility in facilities across the country.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> Why would spine/orthopedic or small biologic companies join the BGC?</h6>
+      <p>
+        <strong><u>Yes, it is possible!</u></strong> The Bone Graft Consortium will be funded by the industry and will be a non-profit entity. Manufacturers who are not listed and reviewed by the BGC may be perceived as less relevant or unwilling to provide unbiased information, risking their acceptance by reviewing hospital/ASC committees. Hospitals will require manufacturers to join the BGC for efficiency and to keep unscrupulous vendors out of their system. The non-profit entity gives confidence and legitimacy to the bone grafting marketplace and allows for complete financial transparency.
+
+
+
+
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> Who will the BGC consist of and how will it help “level the playing field”?
+      </h6>
+      <p>
+      The BCG will recruit board members from bone grafting companies, insurance providers, and key decision makers who will periodically review submissions. The subscription-based model will allow all manufacturers, big or small, to showcase their bone grafting options alongside others, giving them access and visibility in hospital systems and ASCs that they may not have otherwise had due to lack of representation or funding.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> A simple
+        question?
+      </h6>
+      <p>
+        Yes. Go to the billing section of your dashboard and update your payment information.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> A simple
+        question?
+      </h6>
+      <p><strong><u>Unfortunately no</u>.</strong> We do not issue full or partial refunds for any
+        reason.</p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> Another
+        question that is longer than usual</h6>
+      <p>
+        Of course! We’re happy to offer a free plan to anyone who wants to try our service.
+      </p>
+    </div>
+  </div>
+</section>
     </div>
   );
 }
