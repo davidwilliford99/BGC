44b2f1170e05370b0a76153b45c258b8c43973b6
srazam <srazam5@gmail.com>
2023-03-25 21:13:22 -0400

    Update aboutPage.js

--- app/src/pages/aboutPage.js
+++ app/src/pages/aboutPage.js
@@ -6,6 +6,16 @@ function AboutPage(){
             <br></br>
             <h1>About Us</h1>
 
+            <blockquote class="blockquote">
+            <p>BGC is an important company.</p>
+            <footer class="blockquote-footer">Mike Weddington</footer>
+            </blockquote>
+                
+            <div class="container p-5 my-5 bg-dark text-white">
+                <h2>Our Story</h2>
+                <p>Something about the story of BGC. Have a picture here as well.</p> 
+            </div>
+            
             <div class="container p-5 my-5 border">
                 <h2>Our Vision</h2>
                 <p> At The Bone Graft Consortium , we 
@@ -22,12 +32,11 @@ function AboutPage(){
                     not marketing, for best practices and patient specific needs.</p>
             </div>
                 
-
-            <div class="container p-5 my-5 border">
-                <h2>Statements</h2>
-                <p>Not all bone graft is created equally, nor should it have to be. </p>
-                <p>At the Bone Graft Consortium(BGC) we help to take the guess work(and marketing 
-                    confusion) out of the crowded bone grafting space</p>
+            <div class="container p-5 my-5 bg-dark text-white">
+                <h2>Marketing Statements</h2>
+                <p>Not all bone graft is created equally, nor should it have to be. 
+                    At the Bone Graft Consortium(BGC) we help to take the guess work(and marketing 
+                    confusion) out of the crowded bone grafting space.</p>
             </div> 
 
             <div class = "container p-5 my-5 border">      
@@ -37,8 +46,13 @@ function AboutPage(){
                     grafts for your institution’s requirements. This not only helps the hospital, provider, 
                     and patient workflow but it also allows companies to know their place in the queue, without 
                     waiting an undetermined amount of time for a possible approval or denial based on non factual 
-                    vetting processes(current value analysis systems across our country)</p>
+                    vetting processes(current value analysis systems across our country).</p>
             </div>
+
+            <button type="button" class="btn btn-primary btn-md">Browse Products</button> 
+            <br></br>
+            <button type="button" class="btn btn-primary btn-md">Add Your Product</button> 
+
         </div>
     )
 }
