e6c43f8ec2f593b23a15dea748acafb2f3adbe4b
davidwilliford99 <d9899w@gmail.com>
2023-06-06 15:13:51 -0400

    Beginning to rework contact page

--- app/src/pages/ContactPage.js
+++ app/src/pages/ContactPage.js
@@ -1,75 +1,93 @@
 import React, { useState } from 'react';
 
 function ContactForm() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
-
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const response = await fetch('/api/contact/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-
-        if (response.ok) {
-            alert('Thanks for contacting us!');
-            setFormData({
-                name: '',
-                email: '',
-                message: ''
-            });
-        } else {
-            alert('Sorry, there was an error submitting your message.');
-        }
-    };
+    
+    const people = [ 
+        {
+            name: "Michael Weddington",
+            role: "Owner",
+            email: "Michael@weddconsulting.com"
+        },
+        {
+            name: "David Williford",
+            role: "Developer",
+            email: "d9899w@gmail.com"
+        },
+        {
+            name: "Matthew Miranda",
+            role: "Developer",
+            email: "MattMiranda55@gmail.com"
+        },
+    ]
 
     return (
-        <div class="container py-4">
-            <form id='contactForm' onSubmit={handleSubmit}>
-                <div class="mb-3">
-                    <label class="form-label" htmlFor="name">Name:</label>
-                    <input class="form-control"
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <label class="form-label" htmlFor="email">Email:</label>
-                <input class="form-control"
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
 
-                <label class="form-label" htmlFor="message">Message:</label>
-                <textarea class="form-control"
-                    id="message"
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    required
-                ></textarea>
+        <div className='flex gap-5 items-center justify-center py-32'>
 
-                <button class="btn btn-primary btn-lg disabled" type="submit">Submit</button>
-            </form>
+            {
+                people.map((person) => {
+                return  <div className='border-2 rounded-xl p-5 text-center'>
+                            <h1 className='text-2xl'>{person.name}</h1>
+                            <h1 className='py-2 font-bold'>{person.role}</h1>
+                            <h1 className='text-blue-500'>{person.email}</h1>
+                        </div>
+
+                })
+            }
+
+
+            
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // <div class="container py-4">
+        //     <form id='contactForm' onSubmit={handleSubmit}>
+        //         <div class="mb-3">
+        //             <label class="form-label" htmlFor="name">Name:</label>
+        //             <input class="form-control"
+        //                 type="text"
+        //                 id="name"
+        //                 name="name"
+        //                 value={formData.name}
+        //                 onChange={handleChange}
+        //                 required
+        //             />
+        //         </div>
+        //         <label class="form-label" htmlFor="email">Email:</label>
+        //         <input class="form-control"
+        //             type="email"
+        //             id="email"
+        //             name="email"
+        //             value={formData.email}
+        //             onChange={handleChange}
+        //             required
+        //         />
+
+        //         <label class="form-label" htmlFor="message">Message:</label>
+        //         <textarea class="form-control"
+        //             id="message"
+        //             name="message"
+        //             value={formData.message}
+        //             onChange={handleChange}
+        //             required
+        //         ></textarea>
+
+        //         <button class="btn btn-primary btn-lg disabled" type="submit">Submit</button>
+        //     </form>
+        // </div>
     );
 }
 
