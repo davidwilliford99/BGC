6037c7ddc77727128655b3de68fd83e7dea4f1fa
davidwilliford99 <d9899w@gmail.com>
2024-02-21 19:24:04 -0500

    Documents null issue (SingleProductPage)

--- app/src/pages/SingleProductPage.js
+++ app/src/pages/SingleProductPage.js
@@ -35,21 +35,39 @@ export default function SingleProductPage(props) {
     fetchGraftData();
     }, [id]);
 
-    console.log(graftData)
+
+    // let documents = graftData.documents
+    // if (graftData.documents == null) {
+    //     documents = [""];
+    // }
+
 
     return (
         <div className='px-20'>
             <div className='flex flex-col lg:flex-row py-10'>
-                <h1>Product Title</h1>
-                <img src=''/>
+                <h1>{graftData.name}</h1>
+                <img src={graftData.image}/>
             </div>
 
-            <div>Description</div>
 
-            <div className='py-10'>
-                <a>Document</a>
-                <a>Document</a>
-                <a>Document</a>
+            <div>{graftData.description}</div>
+
+
+            <div id="document buttons" className="flex flex-wrap items-center justify-center my-8 gap-3">
+              {
+                graftData.documents.map((docLink, index) => { 
+                  return <a 
+                      href={docLink} 
+                      target="_blank"
+                      >
+                      <button 
+                        className="px-3 py-1 bg-neutral-500 mr-10 text-white font-semibold rounded-xl transition mt-2 w-full">
+                          Document {index + 1}
+                      </button>
+                      
+                    </a>
+                })
+              }
             </div>
 
         </div>
