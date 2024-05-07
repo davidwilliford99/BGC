c74ada46e6adaedf021c004ea35626131849a535
davidwilliford99 <d9899w@gmail.com>
2024-05-07 10:59:14 -0400

    Changed NavbarComp item order

--- app/src/components/NavbarComp.js
+++ app/src/components/NavbarComp.js
@@ -76,10 +76,10 @@ function NavScrollExample(props) {
             navbarScroll
           >
             <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href='/products'>Products</Nav.Link>
-            <Nav.Link href='/contact'>Contact</Nav.Link>
             <Nav.Link href='/pricing'>Pricing</Nav.Link>
+            <Nav.Link href='/contact'>Contact</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
 
 
